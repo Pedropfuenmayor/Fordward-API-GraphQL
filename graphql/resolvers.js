65576d1a28f4f378b896bfb59d0d13e599257ee1
Pedro Fuenmayor <pedropfuenmayor@gmail.com>
2022-01-31 11:04:48 +0100

    finish resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,13 @@
+const User = require("../models/users");
 const Project = require("../models/projects");
 const Challenge = require("../models/challenges");
 const OQ = require("../models/OQ");
+const Idea = require("../models/ideas");
+const Action = require("../models/actions");
+const bcrypt = require("bcryptjs");
+const { extractedIds } = require("../util/extractedIds");
+const validator = require("validator");
+const { transformProject } = require("../graphql/merge");
 
 module.exports = {
   Query: {
@@ -13,10 +20,571 @@ module.exports = {
       }
       return projects;
     },
+    users: async function () {
+      const { rows: users } = await User.fetchAll();
+      if (users.length === 0) {
+        const error = new Error("No users found!");
+        error.code = 404;
+        throw error;
+      }
+      return users;
+    },
+    project: async function (_, { project_id }) {
+      const { rows: project } = await Project.findById(project_id);
+
+      if (project.length === 0) {
+        const error = new Error("No project found!");
+        error.code = 404;
+        throw error;
+      }
+      return {
+        ...project[0],
+      };
+    },
+
+    projects: async function (_, { user_id }) {
+      const { rows: projects } = await Project.fetchAll(user_id);
+      if (projects.length === 0) {
+        const error = new Error("No project found!");
+        error.code = 404;
+        throw error;
+      }
+      return projects.map((project) => {
+        return transformProject(project);
+      });
+    },
+
+    challenges: async function (_, { project_id }) {
+      const { rows: challenges } = await Challenge.fetchAll(project_id);
+      if (challenges.length === 0) {
+        const error = new Error("No challenge found!");
+        error.code = 404;
+        throw error;
+      }
+
+      return challenges;
+    },
+
+    challenge: async function (_, { project_id, challenge_id }) {
+      const { rows: challenge } = await Challenge.findById(
+        project_id,
+        challenge_id
+      );
+      if (challenge.length === 0) {
+        const error = new Error("No challenge found!");
+        error.code = 404;
+        throw error;
+      }
+      return {
+        ...challenge[0],
+      };
+    },
+
+    chosenChallenges: async function (_, { project_id }) {
+      const { rows: challenges } = await Challenge.chosenChallenges(project_id);
+      if (challenges.length === 0) {
+        const error = new Error("No challenge found!");
+        error.code = 404;
+        throw error;
+      }
+
+      return challenges;
+    },
+
+    oq: async function (_, { project_id, challenge_id }) {
+      const { rows: oq } = await OQ.fetchOQ(project_id, challenge_id);
+      if (!oq) {
+        const error = new Error("No OQ found!");
+        error.code = 404;
+        throw error;
+      }
+      return oq[0];
+    },
+
+    ideas: async function (_,{ project_id, challenge_id }) {
+        const { rows: ideas } = await Idea.fetchAll(project_id, challenge_id);
+        if (ideas.length === 0) {
+          const error = new Error("No ideas found!");
+          error.code = 404;
+          throw error;
+        }
+    
+        return ideas;
+      },
+
+      idea: async function (_,{ project_id, challenge_id, idea_id }) {
+        const { rows: idea } = await Idea.findById(
+          project_id,
+          challenge_id,
+          idea_id
+        );
+        if (idea.length === 0) {
+          const error = new Error("No idea found!");
+          error.code = 404;
+          throw error;
+        }
+        return {
+          ...idea[0],
+        };
+      },
+
+      chosenIdeas: async function (_,{ project_id, challenge_id }) {
+        const { rows: ideas } = await Idea.chosenIdeas(project_id, challenge_id);
+        if (ideas.length === 0) {
+          const error = new Error("No ideas found!");
+          error.code = 404;
+          throw error;
+        }
+    
+        return ideas;
+      },
+
+      action: async function (_,{ project_id, challenge_id, idea_id }) {
+        const { rows: action } = await Action.findById(
+          project_id,
+          challenge_id,
+          idea_id
+        );
+        if (action.length === 0) {
+          const error = new Error("No action found!");
+          error.code = 404;
+          throw error;
+        }
+        return {
+          ...action[0],
+        };
+      },
+  },
+  Mutation: {
+    createUser: async function (_, { createUserInput }) {
+      const { user_id, email, password } = createUserInput;
+
+      const errors = [];
+      if (!validator.isEmail(email)) {
+        errors.push({ message: "E-Mail is invalid!" });
+      }
+      if (
+        validator.isEmpty(password) ||
+        !validator.isLength(password, { min: 5 })
+      ) {
+        errors.push({ message: "Password too short!" });
+      }
+      if (errors.length > 0) {
+        const error = new Error("Invalid input!");
+        error.data = errors;
+        error.code = 422;
+        throw error;
+      }
+      const existingUser = await User.findByEmail(email);
+
+      if (existingUser.rowCount === 1) {
+        const error = new Error("User exists already!");
+        throw error;
+      }
+      const hashedPw = await bcrypt.hash(password, 12);
+      const user = new User(user_id, email, hashedPw);
+
+      const savedUser = await user.save();
+
+      return { message: "User added succesfully." };
+    },
+    deleteUser: async function (_, { user_id }) {
+      const deletedUser = await User.delete(user_id);
+
+      if (deletedUser.rowCount === 1) {
+        return { message: "User deleted succesfully." };
+      } else {
+        const error = new Error("Invalid input!");
+        error.code = 422;
+        throw error;
+      }
+    },
+    createProject: async function (_,{ createProjectInput }) {
+        const { project_id, project_name, user_id } = createProjectInput;
+    
+        const errors = [];
+    
+        if (validator.isEmpty(project_name.trim())) {
+          errors.push({ message: "Project name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const project = new Project(project_id, project_name, user_id);
+    
+        const savedProject = await project.save();
+    
+        return { message: "Project created succesfully." };
+      },
+    
+      updateProject: async function (_,{ project_id, project_name }) {
+        const errors = [];
+        if (validator.isEmpty(project_name.trim())) {
+          errors.push({ message: "Project name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const updatedProject = await Project.update(project_id, project_name);
+    
+        if (updatedProject.rowCount === 1) {
+          return { message: "Project updated succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+    
+      deleteProject: async function (_,{ project_id }) {
+        const deletedProject = await Project.delete(project_id);
+    
+        if (deletedProject.rowCount === 1) {
+          return { message: "Project deleted succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      createChallenge: async function (_,{ createChallengeInput }) {
+        const { challenge_id, challenge_name, project_id, challenge_type } =
+          createChallengeInput;
+        const errors = [];
+        if (validator.isEmpty(challenge_name.trim())) {
+          errors.push({ message: "Challenge name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const challenge = new Challenge(
+          challenge_id,
+          challenge_name,
+          project_id,
+          challenge_type
+        );
+    
+        const createdChallenge = await challenge.save();
+    
+        return { message: "Challenge created succesfully." };
+      },
+    
+    
+      selectChallenges: async function (_,{ selectedIds, project_id }) {
+        const receivedIds = extractedIds(selectedIds);
+        const updatedChallenges = await Challenge.selectChallenges(
+          receivedIds,
+          project_id
+        );
+        // refactor to handle error properly
+        if (updatedChallenges.rowCount > 0) {
+          return { message: "Challenges selected succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      
+    
+      updateChallenge: async function (_,{ challenge_id, challenge_name }) {
+        const errors = [];
+        if (validator.isEmpty(challenge_name.trim())) {
+          errors.push({ message: "Challenge name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const updatedChallenge = await Challenge.update(
+          challenge_id,
+          challenge_name
+        );
+    
+        if (updatedChallenge.rowCount === 1) {
+          return { message: "Challenge update succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+    
+      deleteChallenge: async function (_,{ challenge_id }) {
+        const updatedChallenge = await Challenge.delete(challenge_id);
+    
+        if (updatedChallenge.rowCount === 1) {
+          return { message: "Challenge deleted succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      createOQ: async function (_,{ challenge_id, oq_id, oq_name }) {
+        const errors = [];
+        if (validator.isEmpty(oq_name.trim())) {
+          errors.push({ message: "OQ name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const oq = new OQ(oq_id, oq_name, challenge_id);
+    
+        const createdOQ = await oq.save();
+    
+        return { message: "OQ created succesfully." };
+      },
+     
+      updateOQ: async function (_,{ oq_id, oq_name }) {
+        const errors = [];
+        if (validator.isEmpty(oq_name.trim())) {
+          errors.push({ message: "OQ name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const updatedOQ = await OQ.update(oq_id, oq_name);
+    
+        if (updatedOQ.rowCount === 1) {
+          return { message: "OQ updated succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      deleteOQ: async function (_,{ oq_id }) {
+        const updatedOQ = await OQ.delete(oq_id);
+    
+        if (updatedOQ.rowCount === 1) {
+          return { message: "OQ deleted succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      createIdea: async function (_,{ challenge_id, idea_id, idea_name }) {
+        const errors = [];
+        if (validator.isEmpty(idea_name.trim())) {
+          errors.push({ message: "Idea name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const idea = new Idea(idea_id, idea_name, challenge_id);
+    
+        const createdIdea = await idea.save();
+    
+        return { message: "Idea created succesfully." };
+      },
+      
+    
+      selectIdeas: async function (_,{ selectedIds, challenge_id }) {
+        const receivedIds = extractedIds(selectedIds);
+        // refactor to handle error properly
+        const updatedChallenges = await Idea.selectIdeas(receivedIds, challenge_id);
+        if (updatedChallenges.rowCount > 0) {
+          return { message: "Ideas selected succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+     
+    
+      updateIdea: async function (_,{ idea_id, idea_name }) {
+        const errors = [];
+    
+        if (validator.isEmpty(idea_name.trim())) {
+          errors.push({ message: "Idea name must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input.");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const updatedIdea = await Idea.update(idea_id, idea_name);
+    
+        if (updatedIdea.rowCount === 1) {
+          return { message: "Idea updated succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+    
+      updateIdeaImpactEffort: async function (_,
+        { idea_id, idea_impact_effort, impact_effort_value }
+      ) {
+        const updatedIdea = await Idea.updateImpactEffort(
+          idea_id,
+          impact_effort_value,
+          idea_impact_effort
+        );
+    
+        if (updatedIdea.rowCount === 1) {
+          return { message: "Idea updated succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+    
+      deleteIdea: async function (_,{ idea_id }) {
+        const updatedIdea = await Idea.delete(idea_id);
+    
+        if (updatedIdea.rowCount === 1) {
+          return { message: "Idea deleted succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+      createAction: async function (_,{ createActionInput }) {
+        const {
+          action_id,
+          action_what,
+          action_due_date,
+          action_test_until,
+          action_succes_criteria,
+          idea_id,
+        } = createActionInput;
+    
+        const errors = [];
+        if (validator.isEmpty(action_what.trim())) {
+          errors.push({
+            message: "Action 'What need to be donde..' must not be empty!",
+          });
+        }
+        if (!validator.isAfter(action_due_date)) {
+          errors.push({ message: "Action due date must be later than today!" });
+        }
+        if (!validator.isAfter(action_test_until, action_due_date)) {
+          errors.push({ message: "Action test date must be later than due date!" });
+        }
+        if (validator.isEmpty(action_succes_criteria.trim())) {
+          errors.push({ message: "Action 'Succes criteria' must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const action = new Action(
+          action_id,
+          action_what,
+          action_due_date,
+          action_test_until,
+          action_succes_criteria,
+          idea_id
+        );
+    
+        const createdAction = await action.save();
+    
+        return { message: "Action created succesfully." };
+      },
+      
+    
+      updateAction: async function (_,{ updateActionInput }) {
+        const {
+          action_id,
+          action_what,
+          action_due_date,
+          action_test_until,
+          action_succes_criteria,
+        } = updateActionInput;
+    
+        const errors = [];
+        if (validator.isEmpty(action_what.trim())) {
+          errors.push({
+            message: "Action 'What need to be donde..' must not be empty!",
+          });
+        }
+        if (!validator.isAfter(action_due_date)) {
+          errors.push({ message: "Action due date must be later than today!" });
+        }
+        if (!validator.isAfter(action_test_until, action_due_date)) {
+          errors.push({ message: "Action test date must be later than due date!" });
+        }
+        if (validator.isEmpty(action_succes_criteria.trim())) {
+          errors.push({ message: "Action 'Succes criteria' must not be empty!" });
+        }
+        if (errors.length > 0) {
+          const error = new Error("Invalid input!");
+          error.data = errors;
+          error.code = 422;
+          throw error;
+        }
+    
+        const updatedAction = await Action.update(
+          action_what,
+          action_due_date,
+          action_test_until,
+          action_succes_criteria,
+          action_id
+        );
+    
+        if (updatedAction.rowCount === 1) {
+          return { message: "Action updated succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
+    
+      deleteAction: async function (_,{ action_id }) {
+        const deletedAction = await Action.delete(action_id);
+    
+        if (deletedAction.rowCount === 1) {
+          return { message: "Action deleted succesfully." };
+        } else {
+          const error = new Error("Invalid input!");
+          error.code = 422;
+          throw error;
+        }
+      },
   },
   Project: {
     challenges: async function ({ project_id }, args, context) {
-        const { challengeLoader } = context;
+      const { challengeLoader } = context;
       const challenges = challengeLoader.load(project_id);
       if (!challenges) {
         const error = new Error("No challenges found!");
@@ -38,26 +606,26 @@ module.exports = {
       return OQ;
     },
     ideas: async function ({ challenge_id }, args, context) {
-        const { ideasLoader } = context;
-        const ideas = ideasLoader.load(challenge_id);
-        if (!ideas) {
-          const error = new Error("No ideas found!");
-          error.code = 404;
-          throw error;
-        }
-        return ideas;
-      },
+      const { ideasLoader } = context;
+      const ideas = ideasLoader.load(challenge_id);
+      if (!ideas) {
+        const error = new Error("No ideas found!");
+        error.code = 404;
+        throw error;
+      }
+      return ideas;
+    },
   },
-  Idea:{
-      action: async function ({ idea_id }, args, context) {
-        const {  actionsLoader } = context;
-        const action =  actionsLoader.load(idea_id);
-        if (!action) {
-          const error = new Error("No action found!");
-          error.code = 404;
-          throw error;
-        }
-        return action ;
-      },
-  }
+  Idea: {
+    action: async function ({ idea_id }, args, context) {
+      const { actionsLoader } = context;
+      const action = actionsLoader.load(idea_id);
+      if (!action) {
+        const error = new Error("No action found!");
+        error.code = 404;
+        throw error;
+      }
+      return action;
+    },
+  },
 };
